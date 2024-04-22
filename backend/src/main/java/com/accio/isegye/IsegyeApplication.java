41bb615cfa69680e7c7e86cfbd34bb13305b14f0
김기찬 <melti080@naver.com>
2024-04-22 14:43:40 +0900

    :tada: Created SpringBoot Project

---
+++ backend/src/main/java/com/accio/isegye/IsegyeApplication.java
@@ -0,0 +1,13 @@
+package com.accio.isegye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsegyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsegyeApplication.class, args);
+    }
+
+}
