58ed27799eb9766727ef6c352a6bf1e182203d32
오동현 <odh3511@naver.com>
2024-04-24 16:54:08 +0900

    Remove: build folder

--- embedded/ROS/display/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linux/S10P31A706/embedded/ROS/display
-# Build directory: /home/linux/S10P31A706/embedded/ROS/display/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- embedded/ROS/test_ws/build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/linux/S10P31A706/embedded/ROS/test_ws/install/my_package'
