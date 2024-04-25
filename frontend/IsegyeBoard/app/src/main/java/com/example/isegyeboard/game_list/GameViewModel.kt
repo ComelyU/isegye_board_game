dd9b582514dc41af1f0200691cbbe12a0048a0b7
박모은 <moeun9309@gmail.com>
2024-04-25 10:30:54 +0900

    Feat: game list

---
+++ frontend/IsegyeBoard/app/src/main/java/com/example/isegyeboard/game_list/GameViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.isegyeboard.game_list
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.isegyeboard.baseapi.BaseApi
+import kotlinx.coroutines.launch
+
+class GameViewModel() : ViewModel() {
+    private val gameNetwork = BaseApi.getInstance().create(GameApi::class.java)
+
+    private val _gameList = MutableLiveData<List<GameClass>>()
+    val gameList: LiveData<List<GameClass>> = _gameList
+
+    fun getCurrentGameList(storeId: String) {
+        viewModelScope.launch {
+            try {
+                val result = gameNetwork.getGameList(storeId)
+                _gameList.value = result
+            } catch (e: Exception) {
+                // 에러 처리
+                println(e)
+            }
+        }
+    }
+}
--- frontend/IsegyeBoard/app/src/main/java/com/example/isegyeboard/game_list/Gamelist.kt
+++ frontend/IsegyeBoard/app/src/main/java/com/example/isegyeboard/game_list/Gamelist.kt
@@ -1,25 +1,54 @@
 package com.example.isegyeboard.game_list
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.isegyeboard.R
+import kotlinx.coroutines.launch
 
 class Gamelist : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val viewModel: GameViewModel by viewModels()
+    private lateinit var gameListRV: RecyclerView
+    private lateinit var gameAdapter: GameAdapter
+    private lateinit var buttonSearch: ConstraintLayout
+    private lateinit var originalGameList: List<GameClass>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gamelist, container, false)
+        val view = inflater.inflate(R.layout.fragment_gamelist, container, false)
+        gameListRV = view.findViewById(R.id.gameListRV)
+        buttonSearch = view.findViewById(R.id.buttonSearch)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gameAdapter = GameAdapter(requireContext(), emptyList())
+
+        val sharedPreferences = requireActivity().getSharedPreferences("StoreInfo", Context.MODE_PRIVATE)
+        val StoreId = sharedPreferences.getString("StoreId", "")
+
+        gameListRV.layoutManager = GridLayoutManager(requireContext(), 2)
+        gameListRV.adapter = gameAdapter
+
+        viewModel.getCurrentGameList(StoreId!!)
+
+        lifecycleScope.launch {
+            viewModel.gameList.observe(viewLifecycleOwner) { gamelist ->
+                gameAdapter.updateData(gamelist) // 데이터 업데이트
+                originalGameList = gamelist
+            }
+        }
+
+    }
 }
