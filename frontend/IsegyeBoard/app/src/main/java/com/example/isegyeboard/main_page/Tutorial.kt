8f69ad37d6e0c6e90377182d3a5f751a2c9fd6dc
박모은 <moeun9309@gmail.com>
2024-04-26 18:10:00 +0900

    Feat: tutorial and some design

---
+++ frontend/IsegyeBoard/app/src/main/java/com/example/isegyeboard/main_page/Tutorial.kt
@@ -0,0 +1,43 @@
+package com.example.isegyeboard.main_page
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.isegyeboard.R
+
+class Tutorial : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var imageView2: ImageView
+    private lateinit var imageView3: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tutorial)
+
+        imageView = findViewById(R.id.tutorialImage)
+        imageView2 = findViewById(R.id.tutorialImage2)
+        imageView3 = findViewById(R.id.tutorialImage3)
+
+        imageView2.visibility = View.GONE
+        imageView3.visibility = View.GONE
+
+        imageView.setOnClickListener{
+            imageView.visibility = View.GONE
+            imageView2.visibility = View.VISIBLE
+        }
+
+        imageView2.setOnClickListener{
+            imageView2.visibility = View.GONE
+            imageView3.visibility = View.VISIBLE
+        }
+
+        imageView3.setOnClickListener{
+            finish()
+        }
+    }
+}
