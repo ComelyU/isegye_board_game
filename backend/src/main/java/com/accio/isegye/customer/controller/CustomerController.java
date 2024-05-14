7ecff6725f0ab8751970f624664e878c4c412a8c
김기찬 <melti080@naver.com>
2024-05-14 14:07:24 +0900

    Fixed: change topic to turblebot+ID

--- backend/src/main/java/com/accio/isegye/customer/controller/CustomerController.java
+++ backend/src/main/java/com/accio/isegye/customer/controller/CustomerController.java
@@ -12,6 +12,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,7 +21,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -83,6 +86,16 @@ public class CustomerController {
         int roomId = customerService.findRoom(customerId);
         mqttGateway.sendToMqtt(volume, "display/" + roomId);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(path="/{customerId}/swapface", consumes={MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> swapFace(@PathVariable Integer customerId, @RequestPart MultipartFile sourceImg){
+//        customerService
+//            return ResponseEntity.ok()
+//                .contentType(MediaType.valueOf("image/jpg"))
+//                .body();
+//
+        return null;
     }
 
 }
