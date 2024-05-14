2c9028bf74373f565d5ec18538b83c864aabd348
김기찬 <melti080@naver.com>
2024-05-14 17:00:21 +0900

    Feature : start ai project
    Former-commit-id: 2838b73ee792383ef11538988b1b55a8e44a2368

---
+++ ai/main.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import FileResponse, Response
+from pydantic import BaseModel
+from swap_face import swap
+import base64
+
+
+class CreateImageRequest(BaseModel):
+    sourceFile: str
+    themeFile: str
+
+app = FastAPI()
+
+@app.post("/ai/swap")
+async def create_swap_image(files: CreateImageRequest):
+    if not files.sourceFile:
+        return {"message": "No file sent"}
+    
+    sourceFile = base64.b64decode(files.sourceFile)
+    themeFile = base64.b64decode(files.themeFile)   
+    
+    newFile = swap(sourceFile, themeFile)
+
+    return newFile
