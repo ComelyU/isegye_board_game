6a0cd3a074c4f836fa1b207cc93d5601e5f3df70
김기찬 <melti080@naver.com>
2024-05-16 16:15:07 +0900

    Feature: add game recommendation
    Former-commit-id: 23ee12b865c8a5b5e078928da55e786368d20d94

--- ai/main.py
+++ ai/main.py
@@ -1,8 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import FileResponse, Response
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from swap_face import swap
+from recommendation import get_recommendations, get_list
 import base64
 
 
@@ -10,20 +10,36 @@ class CreateImageRequest(BaseModel):
     sourceFile: str
     themeFile: str
 
+
 app = FastAPI()
+
 
 @app.get("/health")
 def check_health():
     return {"status": "ok"}
+
 
 @app.post("/ai/swap")
 async def create_swap_image(files: CreateImageRequest):
     if not files.sourceFile:
         return {"message": "No file sent"}
     
-    sourceFile = base64.b64decode(files.sourceFile)
-    themeFile = base64.b64decode(files.themeFile)   
+    source_file = base64.b64decode(files.sourceFile)
+    theme_file = base64.b64decode(files.themeFile)
     
-    newFile = swap(sourceFile, themeFile)
+    new_file = swap(source_file, theme_file)
 
-    return newFile
+    return new_file
+
+
+@app.post("/ai/recommendation")
+async def get_recommendation(game_id: int):
+    if game_id:
+        if game_id not in get_list():
+            raise HTTPException(
+                status_code=404,
+                detail="Game not found"
+            )
+        return get_recommendations(int(game_id))
+
+    return
